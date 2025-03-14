a60946d908b400735bf68f36ec9e722d64472e53
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-03-14 16:24:59 +0530

    first commit
    first commit : installation and setup of next app

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Parallax from "./components/parallax";
+import parallax from "./components/parallax";
+export default function Home() {
+  return (
+    <>
+    <Parallax/>
+    </>
+  
+  );
+}
