079291dd42e6071ffea3f5c2a60104247f1f2638
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-03-14 18:34:50 +0530

    changes
    changes

--- src/app/components/parallax.tsx
+++ src/app/components/parallax.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useRef } from 'react'
-import Image from 'next/image';
 import { motion , useScroll , useTransform } from "framer-motion";
 
 const Parallax = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { Inter } from "next/font/google";
 import Footer from "./components/footer";
 import Parallax from "./components/parallax";
-import parallax from "./components/parallax";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -17,11 +16,11 @@ export default function Home() {
             <h3 className='text-center mt-20 md:mt-0 text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-900'>Parallax Web Design</h3>
           </div>
           <p className="text-black text-gray-300 font-bold leading-relaxed tracking-wide">
-            This project is a modern Next.js web page featuring a parallax scrolling effect using Framer Motion's useScroll hook.   <br /><br />
-            Styled with Tailwind CSS, it ensures a sleek, responsive design.   <br /><br />
+            This project is a modern Next.js web page featuring a parallax scrolling effect using Framer Motion&apos;s useScroll hook.   <br /><br />
+            Styled with Tailwind CSS  it ensures a sleek, responsive design.   <br /><br />
             The page includes a dynamic content section showcasing web development services with well-structured typography.   <br /><br />
-            Next.js enhances performance with server-side rendering, while Framer Motion enables smooth animations.   <br /><br />
-            This project effectively demonstrates how to build an engaging, high-performance web experience with modern technologies like Next.js, Tailwind CSS, and Framer Motion.
+            Next.js enhances performance with server-side rendering while Framer Motion enables smooth animations.   <br /><br />
+            This project effectively demonstrates how to build an engaging high-performance web experience with modern technologies like Next.js, Tailwind CSS, and Framer Motion.
           </p>
 
         </div>
