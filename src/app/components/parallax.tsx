54a2b9e8cc22edc6d126e867ce4b2da9cbf4d77f
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-06-19 20:03:02 +0530

    formating
    formating

--- src/app/components/parallax.tsx
+++ src/app/components/parallax.tsx
@@ -1,47 +1,47 @@
 "use client"
 import React, { useRef } from 'react'
-import { motion , useScroll , useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Parallax = () => {
-    const ref = useRef(null);   
+    const ref = useRef(null);
     const { scrollYProgress } = useScroll({
         target: ref,
         offset: ["start start", "end start"],
     });
 
-const textY = useTransform(scrollYProgress , [0, 1] ,["10%", "900%"]);
+    const textY = useTransform(scrollYProgress, [0, 1], ["10%", "900%"]);
 
-  return (
-  <div 
-  ref={ref}
-  className='w-full h-screen overflow-hidden relative place-items-center'>
+    return (
+        <div
+            ref={ref}
+            className='w-full h-screen overflow-hidden relative place-items-center'>
 
-    <motion.h1 
-    style={{y: textY}}
-    className='font-bold text-white text-6xl relative z-10 pt-13 '>
-       WELCOME
-    </motion.h1>
+            <motion.h1
+                style={{ y: textY }}
+                className='font-bold text-white text-6xl relative z-10 pt-13 '>
+                WELCOME
+            </motion.h1>
 
 
-    <motion.div
-    className='absolute inset-0 z-0'
-    style={{
-        backgroundImage : `url(/sky.jpg)`,
-        backgroundPosition : "center bottom 30%",
-        backgroundSize : "cover"
-    }}
-    />
+            <motion.div
+                className='absolute inset-0 z-0'
+                style={{
+                    backgroundImage: `url(/sky.jpg)`,
+                    backgroundPosition: "center bottom 30%",
+                    backgroundSize: "cover"
+                }}
+            />
 
-<div
-    className='absolute inset-0 z-20'
-    style={{
-        backgroundImage : `url(/mountains.png)`,
-        backgroundPosition : "bottom",
-        backgroundSize : "cover"
-    }}
-    />
-  </div>
-  )
+            <div
+                className='absolute inset-0 z-20'
+                style={{
+                    backgroundImage: `url(/mountains.png)`,
+                    backgroundPosition: "bottom",
+                    backgroundSize: "cover"
+                }}
+            />
+        </div>
+    )
 }
 
 export default Parallax
